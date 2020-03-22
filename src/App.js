faf6fd7bdee6c7cd73135f08c7bd1fe87486bc2c
BillGeoghegan <billgeoghegan92@gmail.com>
2020-03-22 22:23:59 +0000

    Init commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,79 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import LDClient from "ldclient-js";
+import "./App.css";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      myFeature: "loading",
+      country: undefined
+    };
+  }
+
+  getCountry() {}
+
+  componentDidMount() {
+    // Get Location Information
+    fetch("https://ipapi.co/json/")
+      .then(response => {
+        return response.json();
+      })
+      .then(res => {
+        // Providing settings
+        const endUserSettings = {
+          key: "122070",
+          country: "Spain",
+          custom: {
+            over25: true
+          }
+        };
+
+        // Custom launch-darkly profile settings
+        this.ldclient = LDClient.initialize(
+          "5e77c7b99a7c990760ea5405",
+          endUserSettings
+        );
+
+        // Kicking off launchdarkly
+        this.ldclient.on("ready", this.setFeatureFlag.bind(this));
+      });
+  }
+
+  setFeatureFlag() {
+    // Updating state with response
+    this.setState({
+      myFeature: this.ldclient.variation("country-test")
+    });
+  }
+
+  render() {
+    const { myFeature } = this.state;
+
+    if (myFeature === "loading") {
+      return <div className="App">Loading....</div>;
+    }
+
+    return (
+      <div className="App">
+        <div className="features">
+          <h1>
+            {myFeature === true
+              ? "The feature flag is enabled!"
+              : "The feature flag is NOT enabled!"}
+          </h1>
+          <p>
+            Try hit this with a VPN set to <strong>England</strong>, and it will{" "}
+            <strong>disable</strong> the feature flag.
+          </p>
+          <p>
+            Try hit this with a VPN set to <strong>Spain</strong>, and it will
+            work <strong>50% of the time</strong>.
+          </p>
+        </div>
+      </div>
+    );
+  }
 }
 
 export default App;
